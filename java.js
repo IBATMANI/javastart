1f96d8bd8809c9c5a1ffe109c209e2c9ab77c950
IBATMANI <abasinsafi@hotmail.com>
2022-08-24 09:44:17 +0200

    Var med x y z

--- java.js
+++ java.js
@@ -3,5 +3,17 @@ document.body.style.backgroundColor = "yellow";
 
 
 // Text til siden, som finder byId
-document.getElementById('java').style.fontSize = '1000px';
+document.getElementById('java').style.fontSize = '100px';
+
+document.getElementById("x").innerHTML = "x = 500 ";
+document.getElementById("y").innerHTML = "y = 2000: ";
+
+document.getElementById('x').style.fontSize = '100px';
+document.getElementById('y').style.fontSize = '100px';
+
+var x = 500;
+var y = 2000;
+var z = x + y;
+document.getElementById("z").innerHTML = "Resultat af z er: " + z;
+document.getElementById('z').style.fontSize = '100px';
 
