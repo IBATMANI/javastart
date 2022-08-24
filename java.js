acdc1f2543c8afcaff0d56fd6ae52ab27f2ad517
IBATMANI <abasinsafi@hotmail.com>
2022-08-24 10:45:40 +0200

    navn

--- java.js
+++ java.js
@@ -27,3 +27,23 @@ document.getElementById("z").innerHTML = "Resultat af z er: " + z;
 // Textstoerrelse til z
 document.getElementById('z').style.fontSize = '100px';
 
+// Text til x og y
+document.getElementById("a").innerHTML = "Abasin";
+document.getElementById("b").innerHTML = "Safi";
+
+// Textstoerrelse til x og y
+document.getElementById('a').style.fontSize = '100px';
+document.getElementById('b').style.fontSize = '100px';
+
+// variabler, x, y og z har fået en værdi
+var a = "Abasin";
+var b = "Safi";
+var d = " "
+var c = a + d + b;
+
+// Text til z og z-vaerdien bliver visses selv
+document.getElementById("c").innerHTML = "Fulde navn: " + c;
+
+// Textstoerrelse til z
+document.getElementById('c').style.fontSize = '100px';
+document.getElementById('c').style.color = 'pink';
