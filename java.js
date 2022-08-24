e0e244578a23b1970d8650fe5e41bea41681a117
IBATMANI <abasinsafi@hotmail.com>
2022-08-24 10:09:55 +0200

    script til javascript filen

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
+alert("Månen er grøn")
+alert("Mars er hvid")
+
 // background farve skifter
 document.body.style.backgroundColor = "yellow";
 
@@ -5,15 +8,22 @@ document.body.style.backgroundColor = "yellow";
 // Text til siden, som finder byId
 document.getElementById('java').style.fontSize = '100px';
 
+// Text til x og y
 document.getElementById("x").innerHTML = "x = 500 ";
 document.getElementById("y").innerHTML = "y = 2000: ";
 
+// Textstoerrelse til x og y
 document.getElementById('x').style.fontSize = '100px';
 document.getElementById('y').style.fontSize = '100px';
 
+// variabler, x, y og z har fået en værdi
 var x = 500;
 var y = 2000;
 var z = x + y;
+
+// Text til z og z-vaerdien bliver visses selv
 document.getElementById("z").innerHTML = "Resultat af z er: " + z;
+
+// Textstoerrelse til z
 document.getElementById('z').style.fontSize = '100px';
 
