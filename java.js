52883a429bb68f24e769c4e6be03bc529e5fda92
IBATMANI <abasinsafi@hotmail.com>
2022-08-24 09:17:42 +0200

    kommentar

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-
+// background farve skifter
 document.body.style.backgroundColor = "yellow";
 
 
-
+// Text til siden, som finder byId
 document.getElementById('java').style.fontSize = '1000px';
 
